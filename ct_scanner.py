Normalization and dataset display fixes

ct_scanner.py
@@ -37,6 +37,8 @@ class CTScanner:
         self.normalization_iradon_progress_clbk = normalization_iradon_progress_clbk
 
     def set_input_image(self, image):
+        if image.dtype == np.float64:
+            image = (image * 255).astype(np.uint8)
         self.input_image = make_image_square(image)
         self.circumcircle_diameter = int(self.input_image.shape[0] * math.sqrt(2))
         self.scanner_vis_img_padding = (self.circumcircle_diameter - self.input_image.shape[0]) // 2 + 10
@@ -151,7 +153,7 @@ class CTScanner:
     def _visualize_circumcircle(self, image):
         rr, cc = draw.circle_perimeter(r=self.input_image_center, c=self.input_image_center,
                                        radius=int(self.circumcircle_diameter / 2))
-        image[rr, cc] = 1
+        image[rr, cc] = 255
 
     def visualize_sinogram(self):
         singoram_image = np.zeros((self.radon_result.shape[0], self.radon_result.shape[1]), dtype=np.uint8)
@@ -171,7 +173,6 @@ class CTScanner:
             self.normalization_iradon_progress_clbk(i, len(self.iradon_result))
             for j in range(len(self.iradon_result[i])):
                 rec_img[i, j] = normalize(self.iradon_result[i, j], min_val, max_val)
-        # io.imsave("asd.png", rec_img)
         return rec_img
 
     def restart_scanner(self):
@@ -182,18 +183,18 @@ class CTScanner:
 
 def _visualize_emitter(image, coords):
     rr, cc = draw.circle(r=coords[0], c=coords[1], radius=10)
-    image[rr, cc] = 1
+    image[rr, cc] = 255
 
 
 def _visualize_detector(image, coords):
     rr, cc = draw.circle(r=coords[0], c=coords[1], radius=10)
-    image[rr, cc] = 0.5
+    image[rr, cc] = 127
 
 
 def _visualize_scan_lines(image, scan_lines):
     for line in scan_lines:
         for a, b in line:
-            image[a][b] = 1
+            image[a][b] = 255
 
 
 def filter(line):

dicom_handler.py
@@ -25,6 +25,7 @@ def dicom_time_display_to_dataset(t):
 
 
 def dicom_list_files(path):
+    """Used only in testing"""
     dcm_files = {}
     for root, dirs, files in os.walk(path):
         for file in files:
@@ -36,7 +37,13 @@ def dicom_list_files(path):
 def dicom_load(path):
     ds = pydicom.dcmread(path)
     # print(ds)
-    return ds, ds.pixel_array.astype(np.uint8)
+    image = ds.pixel_array
+    # Convert the image data type to uint8
+    if image.dtype != np.uint8:
+        img_max = np.max(image)
+        img_min = np.min(image)
+        image = 255 * (image - img_min) / (img_max - img_min)
+    return ds, image.astype(np.uint8)
 
 
 def dicom_save():

main.py
@@ -70,13 +70,14 @@ class Main:
                 self.dicom_dataset = dicom_handler.dicom_create_new_dataset()
                 # self.gui.dicom_show_frame.pack_forget()
 
-            self.gui.dicom_show_display_dataset(self.dicom_dataset)
             self.gui.display_image(self.input_image, 'input')
             self.ct_scanner.set_input_image(self.input_image)
             # "Restart" the app
             self.ct_scanner.restart_scanner()
             self.gui.dicom_show_list.delete(0, self.gui.dicom_show_list_next_id)
             self.gui.dicom_show_list_next_id = 1
+            self.gui.dicom_show_display_dataset(self.dicom_dataset)
+
             self.gui.display_image(np.zeros((100, 100), dtype=np.uint8), 'simulation_step')
             self.gui.display_image(np.zeros((100, 100), dtype=np.uint8), 'sinogram')
             self.gui.display_image(np.zeros((100, 100), dtype=np.uint8), 'reco_img')
@@ -84,6 +85,8 @@ class Main:
             self.gui.set_total_radon_progress(0)
             self.gui.set_step_iradon_progress(0)
             self.gui.set_total_iradon_progress(0)
+            self.gui.set_step_radon_progress(0)
+            self.gui.set_normalization_radon_progress(0)
             self.gui.set_normalization_iradon_progress(0)
             self.gui.error_label.config(text='')
             self.gui.toggle_button('dicom_edit', True)
@@ -105,6 +108,7 @@ class Main:
         self.gui.set_total_radon_progress(0)
         self.gui.set_step_iradon_progress(0)
         self.gui.set_total_iradon_progress(0)
+        self.gui.set_normalization_radon_progress(0)
         self.gui.set_normalization_iradon_progress(0)
         self.gui.toggle_button('radon', True)
         self.gui.toggle_button('iradon', False)